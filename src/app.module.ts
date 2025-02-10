4b05a19f90a3dad008fc62b15d2163bd90426301
Christian <christian@redbonzai.com>
2025-02-10 11:09:26 -0600

    feat(data-scraper): telemetry service for observe inc metrics

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './domains/database/database.module';
 import { WebScraperModule } from './domains/web-scraper/web-scraper.module';
+import { TelemetryModule } from './domains/telemetry/telemetry.module';
 
 @Module({
-  imports: [DatabaseModule, WebScraperModule],
+  imports: [DatabaseModule, WebScraperModule, TelemetryModule],
   controllers: [AppController],
   providers: [AppService],
 })
